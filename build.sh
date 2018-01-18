c5d96597c89789379e5ced53ed19bd0e59c55aea
Alexandre Parenteau <aubonbeurre@gmail.com>
2018-01-18 10:49:22 +0100

    changes for SDK/ABI

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 
 #set the path to the android ndk here:
-export ANDROID_NDK=/home/declan/Documents/zone/mid/lib/android/crystax/crystax-ndk-10.3.2_2017_03_08
+export ANDROID_NDK=~/Library/Android/sdk/ndk-bundle/
 
 
 
@@ -15,10 +15,10 @@ export NDK_PROJECT_PATH=./
 # see https://developer.android.com/ndk/guides/application_mk.html
 # and https://developer.android.com/ndk/guides/ndk-build.html
 
-ndk-build APP_STL=gnustl_shared     \
-          APP_PLATFORM=android-9    \
-          APP_ABI="armeabi armeabi-v7a"     \
-          NDK_TOOLCHAIN_VERSION=5   \
+ndk-build APP_STL=c++_shared     \
+          APP_PLATFORM=android-26    \
+          APP_ABI="arm64-v8a x86_64"     \
+          NDK_TOOLCHAIN_VERSION=clang
           NDK_PROJECT_PATH=./       \
           $@
 
