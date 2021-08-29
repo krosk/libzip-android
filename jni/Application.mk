9c57c1a4305510ff076236b0598a6c9d039f1318
Alexis He <ahe@slb.com>
2021-08-29 12:58:07 +0200

    Fix: android with stl

--- jni/Application.mk
+++ jni/Application.mk
@@ -4,8 +4,11 @@ APP_MODULES := libzip
 
 APP_ABI      := arm64-v8a x86_64 x86 armeabi-v7a
 APP_PLATFORM := android-21
+
+APP_STL := c++_shared
+APP_STRIP_MODE := none
+
 #APP_CFLAGS += -Wno-error=format-security
-#APP_STL := stlport_shared
 #APP_CPPFLAGS += -fexceptions
 
 #APP_CPPFLAGS += -std=c++14
