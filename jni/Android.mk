1cca6f2bfca29a22dfbf9536f4288ee29b50e826
Alexis He <ahe@slb.com>
2021-08-30 09:01:41 +0200

    Change: download source code instead of including it

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,28 +1,19 @@
-# see https://developer.android.com/ndk/guides/android_mk.html
-
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ECHO := $(shell wget -nc https://github.com/nih-at/libzip/archive/refs/tags/rel-1-2-0.zip; unzip -n rel-1-2-0.zip libzip-rel-1-2-0/lib/* -x *win32* -d $(LOCAL_PATH))
+
 LOCAL_MODULE := libzip
 
- 
 LOCAL_CFLAGS := -DHAVE_CONFIG_H=1
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/gladman-fcrypt
- 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/libzip-rel-1-2-0/lib/gladman-fcrypt
 
-
-MY_PATH=$(LOCAL_PATH)
- 
-# http://falsinsoft.blogspot.de/2014/10/androidmk-include-multiple-source-files.html
-LOCAL_SRC_FILES :=  $(wildcard $(MY_PATH)/*.c)
-LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(MY_PATH)/%=%)
-  
-
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/**/**/*.c)
+#LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/**/**/**/*.c)
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 
 # link library libz
 LOCAL_LDLIBS := -lz
 
 include $(BUILD_SHARED_LIBRARY)
-#include$lude $(BUILD_STATIC_LIBRARY)
