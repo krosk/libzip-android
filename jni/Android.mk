33c5880e4b6b12dd10047045a36ba1b2a170432b
Alexis He <ahe@slb.com>
2021-08-30 09:47:15 +0200

    Change: includes copy moved to Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,18 +2,27 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ECHO := $(shell wget -nc https://github.com/nih-at/libzip/archive/refs/tags/rel-1-2-0.zip; unzip -n rel-1-2-0.zip libzip-rel-1-2-0/lib/* -x *win32* -d $(LOCAL_PATH))
+LIB_FOLDER := libzip-rel-1-2-0/lib
+ECHO := $(shell wget -nc https://github.com/nih-at/libzip/archive/refs/tags/rel-1-2-0.zip; unzip -n rel-1-2-0.zip $(LIB_FOLDER)/* -x *win32* -d $(LOCAL_PATH))
 
 LOCAL_MODULE := libzip
 
 LOCAL_CFLAGS := -DHAVE_CONFIG_H=1
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/libzip-rel-1-2-0/lib/gladman-fcrypt
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(LIB_FOLDER)/gladman-fcrypt
 
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/**/**/*.c)
 #LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/**/**/**/*.c)
 LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 
-# link library libz
 LOCAL_LDLIBS := -lz
 
 include $(BUILD_SHARED_LIBRARY)
+
+ifndef NDK_LIBS_OUT
+NDK_LIBS_OUT := ./libs
+endif
+
+all:
+	[ -e $(NDK_LIBS_OUT)/../include ] || mkdir $(NDK_LIBS_OUT)/../include
+	cp $(LOCAL_PATH)/zipconf.h $(NDK_LIBS_OUT)/../include
+	cp $(LOCAL_PATH)/$(LIB_FOLDER)/zip.h $(NDK_LIBS_OUT)/../include
